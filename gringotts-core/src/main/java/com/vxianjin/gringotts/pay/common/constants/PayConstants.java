b3ab1deb320b7490ecb74d32df8432a6904e930b
fully <fully_zhang@126.com>
2019-05-21 14:25:13 +0800

    增加uat环境文件

--- gringotts-core/src/main/java/com/vxianjin/gringotts/pay/common/constants/PayConstants.java
+++ gringotts-core/src/main/java/com/vxianjin/gringotts/pay/common/constants/PayConstants.java
@@ -1,8 +1,5 @@
 package com.vxianjin.gringotts.pay.common.constants;
 
-
-import com.vxianjin.gringotts.pay.common.util.Configuration;
-
 /**
  * @Author: chenkai
  * @Date: 2018/7/17 14:27
@@ -13,66 +10,65 @@ public class PayConstants {
     /**
      * 商户编号
      */
-    public final static String MERCHANT_NO = Configuration.getInstance().getValue("merchantAccount");
+    public final static String MERCHANT_NO = ""; // Configuration.getInstance().getValue("merchantAccount");
     /**
      * 商户私钥
      */
-    public final static String MERCHANT_PRIVATE_KEY = Configuration.getInstance().getValue("merchantPrivateKey");
+    public final static String MERCHANT_PRIVATE_KEY = ""; // Configuration.getInstance().getValue("merchantPrivateKey");
     /**
      * 商户私钥
      */
-    public final static String HMAC_KEY = Configuration.getInstance().getValue("hmacKey");
-    public final static String YEEPAY_PUBLIC_KEY = Configuration.getInstance().getValue("yeepayPublicKey");
-    public final static String GROUP_ID = Configuration.getInstance().getValue("groupId");
-    /*===================================yeepay.properties========================================**/
+    public final static String HMAC_KEY = ""; // Configuration.getInstance().getValue("hmacKey");
+    public final static String YEEPAY_PUBLIC_KEY = ""; // Configuration.getInstance().getValue("yeepayPublicKey");
+    public final static String GROUP_ID = ""; // Configuration.getInstance().getValue("groupId");
     /**
      * 充值记录查询
      */
-    public final static String REPAY_RECORD_QUERY_REQURL = Configuration.getInstance().getValue("payRecordQueryURL");
+    public final static String REPAY_RECORD_QUERY_REQURL = ""; // Configuration.getInstance().getValue("payRecordQueryURL");
     /**
      * 批量查询明细的相关变量
      */
-    public final static String BATCH_DETAIL_QUERY_REQURL = Configuration.getInstance().getValue("batchDetailQueryURL");
+    public final static String BATCH_DETAIL_QUERY_REQURL = ""; // Configuration.getInstance().getValue("batchDetailQueryURL");
     /**
      * 有短验绑卡请求接口
      */
-    public final static String BIND_CARDREQUEST_URL =Configuration.getInstance().getValue("bindCardRequestURL");
+    public final static String BIND_CARDREQUEST_URL = ""; //Configuration.getInstance().getValue("bindCardRequestURL");
     /**
      * 有短验绑卡请求确认接口
      */
-    public final static String BIND_CARDCONFIRM_URL = Configuration.getInstance().getValue("bindCardConfirmURL");
+    public final static String BIND_CARDCONFIRM_URL = ""; // Configuration.getInstance().getValue("bindCardConfirmURL");
     /**
      * 有短验绑卡请求重发短验接口
      */
-    public final static String BIND_CARDRESENDSMS_URL =  Configuration.getInstance().getValue("bindCardResendsmsURL");
+    public final static String BIND_CARDRESENDSMS_URL = ""; //  Configuration.getInstance().getValue("bindCardResendsmsURL");
     /**
      * 无短验充值接口
      */
-    public final static String UNSENDBIND_PAY_REQUEST_URL =  Configuration.getInstance().getValue("unSendBindPayRequestURL");
+    public final static String UNSENDBIND_PAY_REQUEST_URL = ""; //  Configuration.getInstance().getValue("unSendBindPayRequestURL");
     /**
      * 无短验绑卡接口
      */
-    public final static String UNSENDBIND_CARD_REQUEST_URL = Configuration.getInstance().getValue("unSendBindCardRequestURL");
+    public final static String UNSENDBIND_CARD_REQUEST_URL = ""; // Configuration.getInstance().getValue("unSendBindCardRequestURL");
 
     /**
      * 代发交易请求地址
      */
-    public final static String REQUEST_URL = Configuration.getInstance().getValue("requestURL");
+    public final static String REQUEST_URL = ""; // Configuration.getInstance().getValue("requestURL");
     /**
      * 主动充值请求接口
      */
-    public final static String AUTOPAY_REQUEST_URL = Configuration.getInstance().getValue("autoPayRequestURL");
+    public final static String AUTOPAY_REQUEST_URL = ""; // Configuration.getInstance().getValue("autoPayRequestURL");
     /**
      * 主动充值短信重复接口
      */
-    public final static String AUTOPAY_RESENDSMS_URL = Configuration.getInstance().getValue("autoPayResendsmsURL");
+    public final static String AUTOPAY_RESENDSMS_URL = ""; // Configuration.getInstance().getValue("autoPayResendsmsURL");
 
     /**
      * 主动充值确认接口
      */
-    public final static String AUTOPAY_CONFIRM_URL = Configuration.getInstance().getValue("autoPayConfirmURL");
+    public final static String AUTOPAY_CONFIRM_URL = ""; // Configuration.getInstance().getValue("autoPayConfirmURL");
 
 
     /**===================================Yeepay.hmacs=========================================**/
-    public final static String SIANG_CERT = Configuration.getInstance().getValue("signCert");
+    public final static String SIANG_CERT = ""; // Configuration.getInstance().getValue("signCert");
 }
