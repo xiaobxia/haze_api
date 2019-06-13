28fad3bdc1da5d5d5e5d4a9401579148acb9188b
fully <fully_zhang@126.com>
2019-06-14 01:00:38 +0800

    银行卡

--- gringotts-core/src/main/java/com/vxianjin/gringotts/pay/component/impl/FuiouServiceImpl.java
+++ gringotts-core/src/main/java/com/vxianjin/gringotts/pay/component/impl/FuiouServiceImpl.java
@@ -400,6 +400,9 @@ public class FuiouServiceImpl implements FuiouService {
             cardInfoSave.setType(UserCardInfo.TYPE_DEBIT);
             cardInfoSave.setUpdateTime(new Date());
             cardInfoSave.setIsBand(1);
+            if (userService.defaultCardCount(Integer.parseInt(userId)) <= 0) {
+                cardInfoSave.setCardDefault(1);
+            }
             boolean flag = userBankService.updateUserBankCard(cardInfoSave);
 
             log.info("FuiouService updateUserBankInfo userId=" + userId + " old flag=" + flag);
